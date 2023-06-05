2ea68e97b2887ff9811d4b8c6af3fc84282d0d29
@Rebeca <>
2023-06-05 15:40:26 -0600

    Marvel Stratego

--- src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
+++ src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
@@ -1,9 +1,17 @@
 
 package grupo7_poryectofinalsm;
 
+/**
+ *
+ * @author fampa
+ */
 public class Grupo7_PoryectoFinalSM {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("HOlaaa");
+        // TODO code application logic here
     }
     
 }
