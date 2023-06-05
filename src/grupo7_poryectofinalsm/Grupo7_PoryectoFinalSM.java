ab9165b8195286e4bae122f9ff67e7678853be54
fampa <fampa@RebecaLT>
2023-06-05 15:45:02 -0600

    Marvel Stratego

--- src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
+++ src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
@@ -12,6 +12,8 @@ public class Grupo7_PoryectoFinalSM {
      */
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("NO ME SIRVEE");
+        
     }
     
 }
