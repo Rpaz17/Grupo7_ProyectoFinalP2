020c670b453a00e3effbc16e3654a035b6aad0f3
pcast <pcast@DESK-E>
2023-06-05 15:39:03 -0600

    MARVEL STRATEGO

--- src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
+++ src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
@@ -11,6 +11,7 @@ public class Grupo7_PoryectoFinalSM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("HOLA SOY TASHA");
         // TODO code application logic here
     }
     
