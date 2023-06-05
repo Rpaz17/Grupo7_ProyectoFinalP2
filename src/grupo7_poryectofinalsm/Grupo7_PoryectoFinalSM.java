e74450bc1beffa05b31cbaf297a2e58029bcf804
pcast <pcast@DESK-E>
2023-06-05 15:45:19 -0600

    PRUEBA

--- src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
+++ src/grupo7_poryectofinalsm/Grupo7_PoryectoFinalSM.java
@@ -11,7 +11,9 @@ public class Grupo7_PoryectoFinalSM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLA SOY TASHA");
+        System.out.println("HOLA SOY TASHA ");
+        System.out.println("FUNCIONA?");
+                
         // TODO code application logic here
     }
     
