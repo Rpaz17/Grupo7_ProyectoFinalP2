0b60585ea833c53b967cb431526eec8bf6b8672e
fampa <fampa@RebecaLT>
2023-06-06 21:26:16 -0600

    boton inicio principal

--- src/grupo7_poryectofinalsm/Menu_Principal.java
+++ src/grupo7_poryectofinalsm/Menu_Principal.java
@@ -3,30 +3,53 @@ package grupo7_poryectofinalsm;
 import java.awt.Graphics;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JFrame;
 
-public class Menu_Principal extends javax.swing.JPanel {
-
+public class Menu_Principal extends javax.swing.JFrame {
+    
     public Menu_Principal() {
         initComponents();
+        this.setContentPane(new Imagen_Inicio());
+        
+    }    
+  
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Menu_Principal().setVisible(true);
+            }
+    
+        });
     }
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        jToggleButton1 = new javax.swing.JToggleButton();
+
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jToggleButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jToggleButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
-public class Imagen_Inicio extends JPanel {
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        ImageIcon nuevaImagen = new ImageIcon("C:/Users/fampa/Documents/NetBeansProjects/Grupo7_PoryectoFinalSM/src/Imagenes_rebeca/INICIO PRINCIPAL STRATEGO.png");
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
+
+    public class Imagen_Inicio extends JPanel {
          public void paint(Graphics g) {
         ImageIcon no_disponible = new ImageIcon("C:/Users/fampa/Documents/NetBeansProjects/Grupo7_PoryectoFinalSM/src/Imagenes_rebeca/INICIO PRINCIPAL STRATEGO.png");
         g.drawImage(no_disponible.getImage(), 0, 0, getWidth(), getHeight(), this);
@@ -37,5 +60,6 @@ public class Imagen_Inicio extends JPanel {
         // Variables declaration - do not modify
         // End of variables declaration
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
