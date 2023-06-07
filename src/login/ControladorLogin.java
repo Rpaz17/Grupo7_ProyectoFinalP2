00e106f76ee7a756161edfde94df5fdbcf24617e
fampa <fampa@RebecaLT>
2023-06-07 16:09:53 -0600

    Login actualizado

---
+++ src/login/ControladorLogin.java
@@ -0,0 +1,39 @@
+
+package login;
+
+import login.Persona;
+
+public class ControladorLogin {
+
+    private Persona[] arregloPersonas;
+    private int tamaño;
+
+    public ControladorLogin() {
+        arregloPersonas = new Persona[10]; // Tamaño inicial del arreglo
+        tamaño = 0;
+    }
+    public Persona buscarPersona(String nombreUser) {
+        for (int i = 0; i < tamaño; i++) {
+            Persona persona = arregloPersonas[i];
+            if (persona != null) {
+                if (persona.getNombreUser().equals(nombreUser)) {
+                    return persona;
+                
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean agregarPersona(Persona persona) {
+        Persona aux = buscarPersona(persona.getNombreUser());
+        if (aux == null) {
+            if (tamaño < arregloPersonas.length) {
+                arregloPersonas[tamaño] = persona;
+                tamaño++;
+                return true;
+            }
+        }
+        return false;
+    }
+}
