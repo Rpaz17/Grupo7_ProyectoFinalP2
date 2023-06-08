f3cb4af41e9d858ac1452a54fffe7900b1e11d17
pcast <pcast@DESK-E>
2023-06-07 21:35:33 -0600

    prueba_imagen 2

--- src/proyecto/partida_nueva.java
+++ src/proyecto/partida_nueva.java
@@ -126,14 +126,12 @@ public class partida_nueva extends javax.swing.JFrame {
     private javax.swing.JPanel panel_partida;
     // End of variables declaration//GEN-END:variables
     public class ImagenFondo extends JPanel {
-        private static String resource ="\\imagen_STRATEGO\\fondo_partidaNueva.png";
-        URL url = getClass().getResource(resource);
         @Override
         public void paint(Graphics g) {
-            ImageIcon fondo = new ImageIcon(url);  //new ImageIcon("C:\\Users\\fampa\\Documents\\NetBeansProjects\\Grupo7_PoryectoFinalSM\\src\\proyecto\\imagen_STRATEGO\\fondo_partidaNueva.png");
+            ImageIcon fondo = new ImageIcon(getClass().getResource("/proyecto/imagen_STRATEGO/fondo_partidaNueva.png"));  //new ImageIcon("C:\\Users\\fampa\\Documents\\NetBeansProjects\\Grupo7_PoryectoFinalSM\\src\\proyecto\\imagen_STRATEGO\\fondo_partidaNueva.png");
             g.drawImage(fondo.getImage(), 0, 0, getWidth(), getHeight(), this);
             setOpaque(false);
             super.paint(g);
         }
-    }
+    }                  
 }
