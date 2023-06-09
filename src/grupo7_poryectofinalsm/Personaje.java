2a20793c3b2987dc83d4bd4c8a955dfdcc146ea4
fampa <fampa@RebecaLT>
2023-06-08 20:37:57 -0600

    Merge origin/master
    Conflicts:
    	src/proyecto/partida_nueva.form
    	src/proyecto/partida_nueva.java

--- src/grupo7_poryectofinalsm/Personaje.java
+++ src/grupo7_poryectofinalsm/Personaje.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package login;
+package grupo7_poryectofinalsm;
 
 /**
  *
- * @author HP
+ * @author fampa
  */
-public class NewClass {
+public class Personaje {
     
 }
