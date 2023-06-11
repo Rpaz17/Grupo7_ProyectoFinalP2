9b635a628345dbcf4447dee1e9b85b8f6f8e5dc9
fampa <fampa@RebecaLT>
2023-06-11 10:49:26 -0600

    Inicializacion de personajes de tablero

--- src/grupo7_poryectofinalsm/Personaje.java
+++ src/grupo7_poryectofinalsm/Personaje.java
@@ -1,4 +1,37 @@
 package grupo7_poryectofinalsm;
 public class Personaje {
-        
+            String MrFantastic_H;
+            String CaptainAmerica_H;
+            String ProfessorX_H;
+            String HighFurry_H;
+            String Spiderman_H;
+            String Wolverine_H;
+            String Namor_H;
+            String Daredevil_H;
+            String SilverSurfer_H;
+            String Hulk_H;
+            String Ironman_H;
+            String Thor_H;
+            String HumanTorch_H;
+            String Cyclops_H;
+            String InvisibleWoman_H;
+            String GhostRider_H;
+            String Punisher_H;
+            String Blade_H;
+            String Thing_H;
+            String EmmaFrost_H;
+            String SheHulk_H;
+            String GiantMan_H;
+            String Beast_H;
+            String Colossus_H;
+            String Gambit_H;
+           String SpiderGirl_H;
+           String Iceman_H;
+           String Storm_H;
+           String Phoenix_H;
+           String DrStrange_H;
+           String Elektra_H;
+           String NigthCrawler_H;
+           String BlackWidow_H;
+           
 }
