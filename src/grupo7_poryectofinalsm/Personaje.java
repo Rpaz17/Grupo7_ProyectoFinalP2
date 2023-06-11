49b06eef70ccf353ea25e53148042d00ee37b250
fampa <fampa@RebecaLT>
2023-06-11 13:50:38 -0600

    Personajes de heores

--- src/grupo7_poryectofinalsm/Personaje.java
+++ src/grupo7_poryectofinalsm/Personaje.java
@@ -1,4 +1,30 @@
 package grupo7_poryectofinalsm;
 public class Personaje {
-                     
+          Personaje MrFantastic_H = new Personaje();
+          Personaje CaptainAmerica_H = new Personaje();
+          Personaje ProfessorX_H = new Personaje();
+          Personaje NickFurry_H = new Personaje();
+          Personaje SpiderMan_H = new Personaje();
+          Personaje Wolverine_H = new Personaje();
+          Personaje Namor_H = new Personaje();
+          Personaje Daredevil_H = new Personaje();
+          Personaje SilverSurfer_H = new Personaje();
+          Personaje Hulk_H = new Personaje();
+          Personaje IronMan_H = new Personaje();
+          Personaje Thor_H = new Personaje();
+          Personaje HumanTorch_H = new Personaje();
+          Personaje Cyclops_H = new Personaje();
+          Personaje InvisibleWoman_H = new Personaje();
+          Personaje GhostRider_H = new Personaje();
+          Personaje Punisher_H = new Personaje();
+          Personaje Blade_H = new Personaje();
+          Personaje Thing_H = new Personaje();
+          Personaje EmmaFrost_H = new Personaje();
+          Personaje SheHulk_H = new Personaje();
+          Personaje GiantMan_H = new Personaje();
+          Personaje Beast_H = new Personaje();
+          Personaje Colossus_H = new Personaje();
+          Personaje Gambit_H = new Personaje();
+          Personaje SpiderGirl_H = new Personaje();
+          
 }
